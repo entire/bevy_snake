7a81540ad2396533bcb94f7a9fb290afb76aaaef
Kosuke Hata <hata.kosuke@gmail.com>
2020-12-15 22:07:06 -1000

    more update

--- src/grid.rs
+++ src/grid.rs
@@ -1,6 +1,6 @@
 
-pub const ARENA_WIDTH: u32 = 10;
-pub const ARENA_HEIGHT: u32 = 10;
+pub const ARENA_WIDTH: u32 = 20;
+pub const ARENA_HEIGHT: u32 = 20;
 
 #[derive(Default, Copy, Clone, Eq, PartialEq, Hash)]
 pub struct Position {
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,23 @@
+#![warn(clippy::complexity)]
 use bevy::prelude::*;
+use bevy::render::pass::ClearColor;
+use std::time::Duration;
+use rand::prelude::random;
 use crate::grid::{Position, Size};
 
 mod grid;
 
-fn main() {
-    App::build()
-        .add_startup_system(setup.system())
-        .add_startup_stage("game_setup")
-        .add_startup_system_to_stage("game_setup", spawn_snake.system())
-        .add_system(snake_movement.system())
-        .add_system(position_translation.system())
-        .add_system(size_scaling.system())
-        .add_plugins(DefaultPlugins)
-        .run();
-}
-
 // setup snake
 
-struct SnakeHead;
+struct SnakeHead {
+    direction:Direction,
+}
+
+struct SnakeMoveTimer(Timer);
+
 struct Materials {
     head_material: Handle<ColorMaterial>,
+    food_material: Handle<ColorMaterial>, 
 }
 
 // setup 2D camera
@@ -28,6 +26,7 @@ fn setup(mut commands: Commands, mut materials: ResMut<Assets<ColorMaterial>>) {
     commands.spawn(Camera2dComponents::default());
     commands.insert_resource(Materials {
         head_material: materials.add(Color::rgb(0.6, 0.6, 0.6).into()),
+        food_material: materials.add(Color::rgb(1.0, 0.0, 1.0).into()),
     });
 }
 
@@ -35,10 +34,12 @@ fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
     commands
         .spawn(SpriteComponents {
             material: materials.head_material.clone(),
-            sprite: Sprite::new(Vec2::new(12.0, 12.0)),
+            sprite: Sprite::new(Vec2::new(10.0, 10.0)),
             ..Default::default()
         })
-        .with(SnakeHead)
+        .with(SnakeHead {
+            direction: Direction::Up,
+        })
         .with(Position {x:3, y:3})
         .with(Size::square(0.8));
 }
@@ -46,22 +47,48 @@ fn spawn_snake(mut commands: Commands, materials: Res<Materials>) {
 // this is a Query type
 fn snake_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut head_positions: Query<With<SnakeHead, &mut Transform>>,
+    snake_timer: ResMut<SnakeMoveTimer>,
+    mut heads: Query<(Entity, &mut SnakeHead)>,
+    mut positions: Query<&mut Position>,
 ) {
-    for mut transform in head_positions.iter_mut() {
-        if keyboard_input.pressed(KeyCode::Left) {
-            *transform.translation.x_mut() -=2.;
+    if let Some((head_entity, mut head)) = heads.iter_mut().next() {
+        let mut head_pos = positions.get_mut(head_entity).unwrap();
+        let dir: Direction = if keyboard_input.pressed(KeyCode::Left) {
+            Direction::Left
+        } else if keyboard_input.pressed(KeyCode::Down) {
+            Direction::Down
+        } else if keyboard_input.pressed(KeyCode::Up) {
+            Direction::Up
+        } else if keyboard_input.pressed(KeyCode::Right) {
+            Direction::Right
+        } else {
+            head.direction
+        };
+        if dir != head.direction.opposite() {
+            head.direction = dir;
         }
-        if keyboard_input.pressed(KeyCode::Right) {
-            *transform.translation.x_mut() +=2.;
+        if !snake_timer.0.finished {
+            return;
         }
-        if keyboard_input.pressed(KeyCode::Down) {
-            *transform.translation.y_mut() -=2.;
-        }
-        if keyboard_input.pressed(KeyCode::Up) {
-            *transform.translation.y_mut() +=2.;
-        }
+        match &head.direction {
+            Direction::Left => {
+                head_pos.x -= 1;
+            }
+            Direction::Right => {
+                head_pos.x += 1;
+            }
+            Direction::Up => {
+                head_pos.y += 1;
+            }
+            Direction::Down => {
+                head_pos.y -= 1;
+            }
+        };
     }
+}
+
+fn snake_timer(time: Res<Time>, mut snake_timer: ResMut<SnakeMoveTimer>) {
+    snake_timer.0.tick(time.delta_seconds);
 }
 
 fn size_scaling(windows: Res<Windows>, mut q: Query<(&Size, &mut Sprite)>) {
@@ -88,3 +115,81 @@ fn position_translation(windows: Res<Windows>, mut q: Query<(&Position, &mut Tra
         );
     }
 }
+
+struct Food;
+
+struct FoodSpawnTimer(Timer);
+impl Default for FoodSpawnTimer {
+    fn default() -> Self {
+        Self(Timer::new(Duration::from_millis(1000), true))
+    }
+}
+
+fn food_spawner(
+    mut commands: Commands,
+    materials: Res<Materials>,
+    time: Res<Time>,
+    mut timer: Local<FoodSpawnTimer>,
+) {
+    timer.0.tick(time.delta_seconds);
+    if timer.0.finished {
+        commands
+            .spawn(SpriteComponents {
+                material: materials.food_material.clone(),
+                ..Default::default()
+            })
+            .with(Food)
+            .with(Position {
+                x: (random::<f32>() * grid::ARENA_WIDTH as f32) as i32,
+                y: (random::<f32>() * grid::ARENA_HEIGHT as f32) as i32,
+            })
+            .with(Size::square(0.8));
+    }
+}
+
+
+
+#[derive(PartialEq, Copy, Clone)]
+enum Direction {
+    Left,
+    Up,
+    Right,
+    Down,
+}
+
+impl Direction {
+    fn opposite(self) -> Self {
+        match self {
+            Self::Right => Self::Left,
+            Self::Up => Self::Down,
+            Self::Down => Self::Up,
+            Self::Left => Self::Right,
+        }
+    }
+}
+
+fn main() {
+    App::build()
+        .add_resource(WindowDescriptor {
+            title: "Snaaaaaaaaake".to_string(),
+            width: 1000,
+            height: 1000,
+            ..Default::default()
+        })
+        .add_resource(ClearColor(Color::rgb(0.04, 0.04, 0.04)))
+        .add_resource(SnakeMoveTimer(Timer::new(
+            Duration::from_millis(150. as u64),
+            true,
+        )))
+        .add_startup_system(setup.system())
+        .add_startup_stage("game_setup")
+        .add_startup_system_to_stage("game_setup", spawn_snake.system())
+        .add_system(snake_movement.system())
+        .add_system(position_translation.system())
+        .add_system(size_scaling.system())
+        .add_system(food_spawner.system())
+        .add_system(snake_timer.system())
+        .add_plugins(DefaultPlugins)
+        .run();
+}
+
